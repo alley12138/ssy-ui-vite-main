a6e3fae0968e17bab5779f51d64a8579840eb16c
173632 <1736423467@qq.com>
2024-10-25 10:33:24 +0800

    xiaoxi

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Unocss from 'unocss/vite'
 import { defineConfig } from 'vite'
-import Unocss from 'unocss/vite';
 
 const rollupOptions = {
   external: ['vue'],
