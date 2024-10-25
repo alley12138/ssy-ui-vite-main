26e6e7527d614617b267d1ff7656b8e0214266fd
173632 <1736423467@qq.com>
2024-10-25 08:38:21 +0800

    Keep calm and commit

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+/// <reference types="vitest/config" />
+
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+import Unocss from './config/unocss'
+
+const rollupOptions = {
+  external: ['vue'],
+  output: {
+    globals: {
+      vue: 'Vue',
+    },
+  },
+}
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler',
+    },
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    Unocss(),
+  ],
+  build: {
+    rollupOptions,
+    minify: false,
+    cssCodeSplit: true,
+    // 添加库模式配置
+    lib: {
+      entry: './src/entry.ts',
+      name: 'SSYUI',
+
+      fileName: 'ssy-ui',
+      // 导出模块格式
+      formats: ['es', 'umd', 'iife'],
+    },
+  },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+  },
+})
