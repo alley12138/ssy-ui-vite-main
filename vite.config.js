5fc05a037c4c0d951deb80b3ebf0db7e6cac3532
173632 <1736423467@qq.com>
2024-10-28 23:07:03 +0800

    feat(custom):

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 /// <reference types="vitest/config" />
 
-import { resolve } from 'path';
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Unocss from 'unocss/vite'
 import { defineConfig } from 'vite'
-
-const rollupOptions = {
-  external: ['vue'],
-  output: {
-    globals: {
-      vue: 'Vue',
-    },
-    exports: "named",
-  },
-}
 
 export default defineConfig({
   resolve: {
@@ -28,40 +18,28 @@ export default defineConfig({
     Unocss(),
   ],
   build: {
-    rollupOptions,
-    minify: false,
-    cssCodeSplit: true,
-    // 添加库模式配置
     lib: {
-      entry: './src/entry.ts',
+      entry: resolve(__dirname, 'src/entry.ts'), // 确保路径正确
       name: 'SSYUI',
-
       fileName: 'ssy-ui',
-      // 导出模块格式
-      formats: ['es', 'umd', 'iife'],
+      formats: ['es', 'umd', 'iife'], // 添加 iife 格式
     },
+    rollupOptions: {
+      external: ['vue'], // 指定外部依赖
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+        exports: 'named',
+      },
+    },
+    minify: 'terser', // 选择压缩工具
+    sourcemap: false, // 根据需要设置为 true
+    reportCompressedSize: true, // 生成压缩大小报告
+    cssCodeSplit: true, // 将 CSS 拆分成单独文件
   },
   test: {
-    // enable jest-like global test APIs
-    globals: true,
-    // simulate DOM with happy-dom
-    // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
-  },
-
-  build: {
-    rollupOptions,
-    minify: "terser", // boolean | 'terser' | 'esbuild'
-    sourcemap: false, // 输出单独 source文件
-    reportCompressedSize: true, // 生成压缩大小报告
-    cssCodeSplit: true,
-    // 添加库模式配置
-    lib: {
-      entry: resolve(__dirname, "src/entry.ts"),
-      name: "SSYUI",
-      fileName: "ssy-ui",
-      // 导出模块格式
-      formats: ["es", "umd"],
-    },
+    globals: true, // 启用类似 Jest 的全局测试 API
+    environment: 'happy-dom', // 使用 happy-dom 模拟 DOM
   },
 })
